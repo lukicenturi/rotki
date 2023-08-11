cbc4582c51cfe0f1192ee01d92b40eb609a3390a
lukicenturi <lukicenturi@gmail.com>
2023-08-11 19:33:24 +0700

    feat: use ui library

--- frontend/app/src/pages/defi/index.vue
+++ frontend/app/src/pages/defi/index.vue
@@ -1,55 +1,5 @@
-<script setup lang="ts">
-import { type ComputedRef } from 'vue';
-import { useAppRoutes } from '@/router/routes';
-import { type TabContent } from '@/types/tabs';
-
-const { appRoutes } = useAppRoutes();
-
-const tabs: ComputedRef<TabContent[]> = computed(() => {
-  const Routes = get(appRoutes);
-  return [
-    Routes.DEFI_OVERVIEW,
-    Routes.DEFI_DEPOSITS,
-    Routes.DEFI_LIABILITIES,
-    Routes.DEFI_AIRDROPS
-  ];
-});
-
-const { defiSetupDone } = storeToRefs(useFrontendSettingsStore());
-
-const defi = useDefiStore();
-const yearn = useYearnStore();
-const aave = useAaveStore();
-const compound = useCompoundStore();
-const maker = useMakerDaoStore();
-const airdrops = useAirdropStore();
-
-const { resetDefiStatus } = useStatusStore();
-
-onUnmounted(() => {
-  defi.$reset();
-  yearn.$reset();
-  aave.$reset();
-  compound.$reset();
-  maker.$reset();
-  airdrops.$reset();
-  resetDefiStatus();
-});
-</script>
-
 <template>
-  <div>
-    <TabNavigation
-      v-if="defiSetupDone"
-      :tab-contents="tabs"
-      no-content-margin
-    />
-    <DefiWizard v-else class="mt-8" />
+  <div class="container">
+    <RouterView />
   </div>
 </template>
-
-<style scoped lang="scss">
-.decentralized-finance {
-  height: 100%;
-}
-</style>
