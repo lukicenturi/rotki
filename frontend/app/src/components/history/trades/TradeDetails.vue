7b24462f5a5eba0a3c0066b37b506b710cae284b
lukicenturi <lukicenturi@gmail.com>
2023-12-05 10:56:40 +0700

    refactor: migrate all icons

--- frontend/app/src/components/history/trades/TradeDetails.vue
+++ frontend/app/src/components/history/trades/TradeDetails.vue
@@ -12,11 +12,7 @@ const props = withDefaults(
 );
 
 const { item } = toRefs(props);
-const { dark } = useTheme();
 const { t } = useI18n();
-
-const link = computed(() => get(item).link || '');
-const { href, hasLink, onLinkClick } = useLinks(link);
 </script>
 
 <template>
@@ -24,52 +20,38 @@ const { href, hasLink, onLinkClick } = useLinks(link);
     <template #title>
       {{ t('closed_trades.details.title') }}
     </template>
-    <VRow>
-      <VCol cols="auto" class="font-medium">
+    <div class="grid grid-cols-[auto_1fr] gap-4">
+      <span class="font-medium">
         {{ t('closed_trades.details.fee') }}
-      </VCol>
-      <VCol>
+      </span>
+
+      <template v-if="!!item.fee">
         <AmountDisplay
-          v-if="!!item.fee"
           class="closed-trades__trade__fee"
           :asset="item.feeCurrency"
           :value="item.fee"
         />
-        <span v-else>-</span>
-      </VCol>
-    </VRow>
-    <VRow align="center">
-      <VCol cols="auto" class="font-medium">
+      </template>
+      <template v-else> - </template>
+
+      <span class="font-medium">
         {{ t('closed_trades.details.link') }}
-      </VCol>
-      <VCol>
-        <span v-if="!item.link">
-          {{ t('closed_trades.details.link_data') }}
+      </span>
+
+      <template v-if="!item.link">
+        {{ t('closed_trades.details.link_data') }}
+      </template>
+
+      <HashLink v-else :text="item.link" />
+
+      <template v-if="item.notes">
+        <span class="font-medium">
+          {{ t('notes_display.notes') }}
         </span>
-        <span v-else>
-          {{ item.link }}
-          <VTooltip v-if="hasLink" top open-delay="600">
-            <template #activator="{ on, attrs }">
-              <VBtn
-                small
-                icon
-                v-bind="attrs"
-                color="primary"
-                class="ml-2"
-                :class="dark ? null : 'grey lighten-4'"
-                :href="href"
-                target="_blank"
-                v-on="on"
-                @click="onLinkClick()"
-              >
-                <VIcon :small="true"> mdi-launch </VIcon>
-              </VBtn>
-            </template>
-            {{ item.link }}
-          </VTooltip>
-        </span>
-      </VCol>
-    </VRow>
-    <NotesDisplay :notes="item.notes" />
+        <div class="break-words">
+          {{ item.notes }}
+        </div>
+      </template>
+    </div>
   </TableExpandContainer>
 </template>
